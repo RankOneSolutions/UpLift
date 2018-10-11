0c000b31bb63f36cc10228a744496a198e64a740
rkoron <rkoron007@gmail.com>
2018-10-10 22:24:56 -0700

    Connect backend to frontend

--- server/server.js
+++ server/server.js
@@ -1,43 +1,45 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
-const keys = require("./config/keys");
+const path = require("path");
+import routes from './routes';
 
-// import path from 'path';
-
-// import routes from './routes';
 
 // for routing our index.html
-// app.use(express.static(path.resolve(__dirname + '../../client')));
-
-// connecting to our router
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-      // accessToken allows you to change things - expires
-      // refreshToken- can update the accessToken
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-    })
-);
-
-//original authenticate request
-app.get('/auth/google', passport.authenticate('google',
-  { scope: ['profile', 'email'] })
-);
-
-//incoming request with code coming back
-app.get('/auth/google/callback', passport.authenticate('google'));
+// __dirname always references where bundle.js is located
+app.use(express.static(path.resolve(__dirname + '../../client')));
+app.use('/', routes);
 
 
 const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+// const keys = require("./config/keys");
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// oAuth Shit
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: '/auth/google/callback'
+//     }, (accessToken, refreshToken, profile, done) => {
+//       // accessToken allows you to change things - expires
+//       // refreshToken- can update the accessToken
+//       console.log(accessToken);
+//       console.log(refreshToken);
+//       console.log(profile);
+//     })
+// );
+
+// //original authenticate request
+// app.get('/auth/google', passport.authenticate('google',
+//   { scope: ['profile', 'email'] })
+// );
+
+// //incoming request with code coming back
+// app.get('/auth/google/callback', passport.authenticate('google'));

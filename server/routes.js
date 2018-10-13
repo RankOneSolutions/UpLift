2673853055864664c6dceb617e7d742d04d19289
Augustine Wittkower <augustine.wittkower@gmail.com>
2018-10-13 03:01:01 -0700

    data

--- server/routes.js
+++ server/routes.js
@@ -15,8 +15,8 @@ router.get('/db', async (req, res) => {
 	    try {
 		          const client = await pool.connect()
 		          const result = await client.query('SELECT * FROM cute_animals');
-		          const results = { 'results': (result) ? result.rows : null};
-		          res.send( results );
+		         
+		          res.send( result );
 		          client.release();
 		        } catch (err) {
 				      console.error(err);

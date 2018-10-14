20591b2738821419cf1242295b0c71e4e3bd4e00
Jacob Gronert <jacobgronert@gmail.com>
2018-10-13 19:42:36 -0700

    added css files and new Header component

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header  from './Header'
+import './app.css'
 
 class App extends Component {
   constructor(props) {
@@ -20,25 +22,29 @@ class App extends Component {
 
     return (
       <div>
-       <h1>I'm in Redux and React Router! Tight.</h1>
-       <h3>Check out some cute animals:</h3>
+       <Header />
 
-        {results.map(row =>
-          <ul>
-            <li>
-              id: {row.id}
-            </li>
-            <li>
-              name: {row.name}
-            </li>
-            <li>
-              species: {row.species}
-            </li>
-            <li>
-              number of legs: {row.leg_count}
-            </li>
-          </ul>
-        )}
+       <div className='content'>
+        <h1>I'm in Redux and React Router! Tight.</h1>
+        <h3>Check out some cute animals:</h3>
+
+          {results.map(row =>
+            <ul>
+              <li>
+                id: {row.id}
+              </li>
+              <li>
+                name: {row.name}
+              </li>
+              <li>
+                species: {row.species}
+              </li>
+              <li>
+                number of legs: {row.leg_count}
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
     );
   }
